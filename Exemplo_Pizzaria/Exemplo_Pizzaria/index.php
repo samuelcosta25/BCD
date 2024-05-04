4b4dc7da7a0249c618645b692f8fd1615f4777f2
samuelcosta25 <samu.live@outlook.com>
2024-05-03 21:58:22 -0300

    03.05.24

---
+++ Exemplo_Pizzaria/Exemplo_Pizzaria/index.php
@@ -0,0 +1,13 @@
+<?php
+include 'functions.php';
+
+?>
+
+<?= template_header('Pizzaria Samuel')?>
+
+<div class="content">
+    <h2>Início</h2>
+    <p>Bem-vindo ao site da Pizzaria Samuel</p>
+    </div>
+
+    <?=template_footer()?>
